dc5071c4cc3568fe32d2f66edc0c9491cfbce2c7
mihir Prasad bhuyan <mihirprasad62@gmail.com>
2026-09-08 10:57:02 +0530

    get all food api done.

--- src/main/java/com/kodewala/controller/FoodController.java
+++ src/main/java/com/kodewala/controller/FoodController.java
@@ -7,14 +7,13 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 import software.amazon.awssdk.thirdparty.jackson.core.JsonProcessingException;
 import tools.jackson.databind.ObjectMapper;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/foods")
@@ -37,5 +36,10 @@ public class FoodController {
         }
         FoodResponse response=foodService.addFood(request,file);
         return response;
+    }
+
+    @GetMapping
+    public List<FoodResponse> readFoods(){
+       return foodService.readFoods();
     }
 }
